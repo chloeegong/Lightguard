84f0e7f3c93cab1ae095f0f2b1e251c0bccfc20a
jocelynGonzalez <75452788+jocelynGonzalez@users.noreply.github.com>
2023-04-20 14:07:27 -0700

    fixing call button and navbar

--- CallEmergServicesPage.js
+++ CallEmergServicesPage.js
@@ -1,5 +1,6 @@
 //jocelyn
 import { Button, ThemeProvider, Input } from '@rneui/themed';
+import { StyleSheet, View, TouchableOpacity, wrap } from "react-native";
 import Icon from 'react-native-vector-icons/FontAwesome';
 import axios from 'axios';
 import React, {useState} from 'react';
@@ -35,7 +36,8 @@ export default function CallEmergServicesPage({ navigation }) {
 //creating simple button for the application
 //onPress uses the initiateCall function to fetch api
     return (
-       
+      //This view encapsulates all of our elements
+      <View style={styles.container}>
         <Button
           title="Call the Emergency Number"
           buttonStyle={{
@@ -47,13 +49,27 @@ export default function CallEmergServicesPage({ navigation }) {
           containerStyle={{
             bottom: "2%",
             width: 200,
-            marginHorizontal: 110,
+            marginHorizontal: 700,
             marginVertical: 350,
           }}
           onPress={() => {
             handleCall();
           }}
           titleStyle={{ fontFamily: "Menlo", fontSize: 18 }} />
+        </View>  
       );
- 
+  
 };
+//Styling used for the view everything is incapsulated in
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "flex-start",
+    changeDirection: "inherit",
+    backgroundColor: "#181818",
+    alignItems: "center",
+    //justifyContent: "space-around",
+  },
+});
+
